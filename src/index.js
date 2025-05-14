1558012b4623a602f8fd7aed4ca413a2a9b9fc12
malakabdelbaki <malakabdelbaki28@gmail.com>
2025-05-14 18:28:46 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,24 @@
+// index.js
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_Eh2seJlBM",
+  client_id: "7kp7g3giro4pav3qo9keq36l2l",
+  redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+  response_type: "code",
+  scope: "email openid phone",
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// wrap the application with AuthProvider
+root.render(
+  <React.StrictMode>
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
+);
